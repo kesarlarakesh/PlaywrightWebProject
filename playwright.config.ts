07f78b82de012a075861159befe4570acf8a1df3
kesarlarakesh <itsrakeshkesarla@gmail.com>
2025-09-18 22:00:43 +0530

    Remove global-teardown.ts as it's no longer needed

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,9 +45,6 @@ export default defineConfig({
   /* Global setup for test run */
   globalSetup: path.join(__dirname, './global-setup.ts'),
   
-  /* Global teardown for test run */
-  globalTeardown: path.join(__dirname, './global-teardown.ts'),
-  
   /* Get headless mode from environment variable or default to true */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
